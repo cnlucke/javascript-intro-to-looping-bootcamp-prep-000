18acb9c9f5cb0f5d70d98b6dd10450cf2ec565f6
Cristy Lucke <cristy.lucke@gmail.com>
2017-09-30 21:33:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   var stringArray[]
-  
+
   for (var i = 0; i < 25; i++) {
     if i === 1 {
       stringArray.push(" I am ${i} strange loop.")
@@ -11,11 +11,21 @@ function forLoop(array) {
   return stringArray
 }
 
-1) loops forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."` to an array 25 times:
-   ReferenceError: forLoop is not defined
-    at Context.it (test/loops-test.js:32:28)
-2) loops whileLoop(n) counts down from n to 0:
-   ReferenceError: whileLoop is not defined
-    at Context.it (test/loops-test.js:50:14)
-3) loops doWhileLoop(array) removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`:
-   ReferenceError: doWhileLoop is not defined
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    console.log(array.pop)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+}
+//loops doWhileLoop(array) removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`:
